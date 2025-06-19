220c143aa336a3365635e91d5186650dce198648
Jeremy Drake <github@jdrake.com>
2025-06-19 11:40:46 -0700

    Revert "Cygwin: signal: Prevent unexpected crash on frequent SIGSEGV"
    This reverts commit f305ca916ad25870fb010334e4fcaf93057c78b9.

--- winsup/cygwin/local_includes/ntdll.h
+++ winsup/cygwin/local_includes/ntdll.h
@@ -1366,8 +1366,7 @@ typedef enum _THREADINFOCLASS
   ThreadBasicInformation = 0,
   ThreadTimes = 1,
   ThreadImpersonationToken = 5,
-  ThreadQuerySetWin32StartAddress = 9,
-  ThreadSuspendCount = 35
+  ThreadQuerySetWin32StartAddress = 9
 } THREADINFOCLASS, *PTHREADINFOCLASS;
 
 typedef struct _THREAD_BASIC_INFORMATION
